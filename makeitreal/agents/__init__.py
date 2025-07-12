d17441116e8b225e069505375672f6d2a3f81c0a
WallaceSchwein <willi.kristen@live.de>
2025-07-12 15:43:33 +0200

    feat(agents): export SpecWriter in package interface

--- makeitreal/agents/__init__.py
+++ makeitreal/agents/__init__.py
@@ -2,5 +2,6 @@
 
 from .base import BaseAgent
 from .idea_curator import IdeaCurator
+from .spec_writer import SpecWriter
 
-__all__ = ["BaseAgent", "IdeaCurator"]
+__all__ = ["BaseAgent", "IdeaCurator", "SpecWriter"]
